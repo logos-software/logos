5ace12809fd2dee0bb79f726fef92181bed5dcc6
Ananias Raphael <ananias.nobrega@solutis.com.br>
2025-05-10 17:39:20 -0300

    feat: Adiciona estrutura inicial para o aplicativo de notas

--- apps/notes/apps.py
+++ apps/notes/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NodesConfig(AppConfig):
+class NotesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.nodes'
+    name = 'apps.notes'
--- apps/notes/urls.py
+++ apps/notes/urls.py
@@ -1,12 +1,12 @@
 """
-URLs para o aplicativo nodes.
+URLs para o aplicativo notes.
 """
 from django.urls import path
 from . import views
 
-app_name = 'nodes'
+app_name = 'notes'
 
 urlpatterns = [
-    # Adicionar URLs do aplicativo nodes aqui
+    # Adicionar URLs do aplicativo notes aqui
     # path('', views.node_list, name='node_list'),
 ]
